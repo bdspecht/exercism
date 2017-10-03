88cadedb997fce32390e5f6e43669a048f1cc4e5
Ben Specht <bdspecht@gmail.com>
2017-10-02 20:19:27 -0500

    Isogram solution for exercism

--- python/isogram/isogram.py
+++ python/isogram/isogram.py
@@ -1,5 +1,14 @@
+'''
+Function that checks if a given string is an isogram
+'''
+
 def is_isogram(string):
-    if len(set(string)) == len(string):
-        return True
-    else:
-        return False
+    '''
+    Args:
+        string - String to check
+    Returns:
+        True or False if the string being checked is considered an isogram
+    '''
+    string_list = [c.lower() for c in string if c.isalnum()]
+
+    return len(set(string_list)) == len(string_list)
